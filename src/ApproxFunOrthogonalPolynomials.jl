0a6a56a67c60626dd1fb57edf16365d1e31896fc
Sheehan Olver <solver@mac.com>
2019-04-22 08:39:12 +0100

    DomainSets.jl v0.0.2

--- src/ApproxFunOrthogonalPolynomials.jl
+++ src/ApproxFunOrthogonalPolynomials.jl
@@ -47,7 +47,7 @@ import ApproxFunBase: normalize!, flipsign, FiniteRange, Fun, MatrixFun, UnsetSp
 
 import DomainSets: Domain, indomain, UnionDomain, ProductDomain, FullSpace, Point, elements, DifferenceDomain,
             Interval, ChebyshevInterval, boundary, ∂, rightendpoint, leftendpoint,
-            dimension, Domain1d, Domain2d   
+            dimension
             
 import BandedMatrices: bandrange, bandshift,
                 inbands_getindex, inbands_setindex!, bandwidth, AbstractBandedMatrix,
