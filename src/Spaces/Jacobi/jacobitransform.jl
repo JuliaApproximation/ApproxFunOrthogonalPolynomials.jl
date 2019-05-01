9b1fbe7efb916bb36455b9b862770017d4dc1cdc
Sheehan Olver <solver@mac.com>
2019-05-01 09:24:24 +0100

    Add type to JacobiTransformPlan

--- src/Spaces/Jacobi/jacobitransform.jl
+++ src/Spaces/Jacobi/jacobitransform.jl
@@ -1,20 +1,27 @@
 
 points(S::Jacobi, n) = points(Chebyshev(domain(S)), n)
 
-struct JacobiTransformPlan{CPLAN,CJT}
+struct JacobiTransformPlan{T,CPLAN,CJT} <: AbstractTransformPlan{T}
     chebplan::CPLAN
     cjtplan::CJT
 end
 
+JacobiTransformPlan(chebplan::CPLAN, cjtplan::CJT) where {CPLAN,CJT} =
+    JacobiTransformPlan{eltype(chebplan),CPLAN,CJT}(chebplan, cjtplan)
+
 plan_transform(S::Jacobi, v::AbstractVector) =
     JacobiTransformPlan(plan_transform(Chebyshev(), v), plan_icjt(v, S.a, S.b))
-*(P::JacobiTransformPlan, vals) = P.cjtplan*(P.chebplan*vals)
+*(P::JacobiTransformPlan, vals::AbstractVector) = P.cjtplan*(P.chebplan*vals)
 
 
-struct JacobiITransformPlan{CPLAN,CJT}
+struct JacobiITransformPlan{T,CPLAN,CJT} <: AbstractTransformPlan{T}
     ichebplan::CPLAN
     icjtplan::CJT
 end
+
+JacobiITransformPlan(chebplan::CPLAN, cjtplan::CJT) where {CPLAN,CJT} =
+    JacobiITransformPlan{eltype(chebplan),CPLAN,CJT}(chebplan, cjtplan)
+
 
 
 plan_itransform(S::Jacobi,v::AbstractVector) =
