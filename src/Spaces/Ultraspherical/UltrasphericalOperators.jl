94df366755b4eeb05fe05ce418e6fa3f3cca73fe
Johannes van den Berg <41743877+Gyoshi@users.noreply.github.com>
2020-04-09 19:11:07 +0200

    Ultraspherical conversion fix error catch

--- src/Spaces/Ultraspherical/UltrasphericalOperators.jl
+++ src/Spaces/Ultraspherical/UltrasphericalOperators.jl
@@ -141,7 +141,7 @@ function Conversion(A::Ultraspherical,B::Ultraspherical)
     a=order(A); b=order(B)
     if b==a
         ConversionWrapper(Operator(I,A))
-    elseif -1 ≤ b-a ≤ 1 && b ≠ 1
+    elseif -1 ≤ b-a ≤ 1 && (a,b) ≠ (2,1)
         ConcreteConversion(A,B)
     elseif b-a > 1
         d=domain(A)
