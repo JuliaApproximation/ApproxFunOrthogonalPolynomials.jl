d95695e1bcda3f7f38c07a5e440da619c7a0132e
Johannes van den Berg <41743877+Gyoshi@users.noreply.github.com>
2020-04-17 09:20:25 +0200

    Ultraspherical conversion error catching (#32)
    * Ultraspherical conversion error catching
    
    Closes #31
    
    * Ultraspherical conversion fix error catch
    
    * Add tests for Ultraspherical conversion argument error

---
+++ test/UltrasphericalTest.jl
@@ -0,0 +1,13 @@
+using ApproxFunOrthogonalPolynomials, ApproxFunBase, Test, SpecialFunctions, LinearAlgebra
+import ApproxFunBase: testbandedbelowoperator, testbandedoperator, testspace, testtransforms, Vec,
+                    maxspace, NoSpace, hasconversion, testfunctional,
+                    reverseorientation, ReverseOrientation
+import ApproxFunOrthogonalPolynomials: jacobip
+
+@testset "Ultraspherical" begin
+    @testset "Conversion" begin
+        # Tests whether invalid/unimplemented arguments correctly throws ArgumentError
+        @test_throws ArgumentError Conversion(Ultraspherical(2), Ultraspherical(1))
+        @test_throws ArgumentError Conversion(Ultraspherical(3), Ultraspherical(1.9))
+    end
+end
