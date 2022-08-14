987df984eeb67107e55043b908d4bf1899bd4fef
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-08-14 19:30:22 +0400

    Inference in Chebyshev/Ultraspherical Conversion (#88)

--- test/UltrasphericalTest.jl
+++ test/UltrasphericalTest.jl
@@ -9,5 +9,24 @@ import ApproxFunOrthogonalPolynomials: jacobip
         # Tests whether invalid/unimplemented arguments correctly throws ArgumentError
         @test_throws ArgumentError Conversion(Ultraspherical(2), Ultraspherical(1))
         @test_throws ArgumentError Conversion(Ultraspherical(3), Ultraspherical(1.9))
+
+        # Conversions from Chebyshev to Ultraspherical should lead to a small union of types
+        Tallowed = Union{
+            ApproxFunBase.ConcreteConversion{
+                Chebyshev{ChebyshevInterval{Float64}, Float64},
+                Ultraspherical{Int64, ChebyshevInterval{Float64}, Float64}, Float64},
+            ApproxFunBase.ConversionWrapper{TimesOperator{Float64, Tuple{Int64, Int64}}, Float64}};
+        @inferred Tallowed Conversion(Chebyshev(), Ultraspherical(1));
+        @inferred Tallowed Conversion(Chebyshev(), Ultraspherical(2));
+        # Conversions between Ultraspherical should lead to a small union of types
+        Tallowed = Union{
+            ApproxFunBase.ConcreteConversion{
+                Ultraspherical{Int64, ChebyshevInterval{Float64}, Float64},
+                Ultraspherical{Int64, ChebyshevInterval{Float64}, Float64}, Float64},
+            ApproxFunBase.ConversionWrapper{
+                ConstantOperator{Float64,
+                    Ultraspherical{Int64, ChebyshevInterval{Float64}, Float64}}, Float64},
+                    ApproxFunBase.ConversionWrapper{TimesOperator{Float64, Tuple{Int64, Int64}}, Float64}};
+        @inferred Tallowed Conversion(Ultraspherical(1), Ultraspherical(2));
     end
 end
