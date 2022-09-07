15d8f144b4db9eff072bd77ea6b29a0162861827
Jishnu Bhattacharya <jishnub@users.noreply.github.com>
2022-09-07 12:08:55 +0400

    bugfix

--- src/precompile.jl
+++ src/precompile.jl
@@ -4,7 +4,6 @@ using SnoopPrecompile
     splist = Any[Jacobi(1, 1), Chebyshev(), Ultraspherical(1)]
     append!(splist, Any[Jacobi(1, 1, 0..1), Chebyshev(0..1), Ultraspherical(1, 0..1)])
     push!(splist, Chebyshev(Segment(1.0+im,2.0+2im)))
-    a = Fun(ChebyshevInterval{BigFloat}(),BigFloat[1,2,3])
     @precompile_all_calls begin
         for S in splist
         	v = [0.0, 1.0]
@@ -15,6 +14,6 @@ using SnoopPrecompile
         	norm(differentiate(cumsum(f))-f)
         	norm(first(cumsum(f)))
         end
-        Fun(sin,Interval(big"0.0", big"1.0")
+        roots(Fun(sin,Interval(big"0.0", big"1.0")))
     end
 end
