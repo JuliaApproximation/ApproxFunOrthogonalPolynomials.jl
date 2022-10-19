f4bc03e60a3e9ad4eb4b4ace9f6d4e88d6fbc7ac
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-10-19 21:48:32 +0400

    rearrange precompile statements

--- src/precompile.jl
+++ src/precompile.jl
@@ -5,16 +5,14 @@ using SnoopPrecompile
     append!(splist, Any[Jacobi(1, 1, 0..1), Chebyshev(0..1), Ultraspherical(1, 0..1)])
     push!(splist, Chebyshev(Segment(1.0+im,2.0+2im)))
     # special functions
-    spfns = Any[sin, cos, exp]
+    spfns = (sin, cos, exp)
+    v = ones(2)
     @precompile_all_calls begin
         for S in splist
-        	v = [0.0, 1.0]
         	f = Fun(S,v)
             1/(f^2+1)
         	abs(DefiniteIntegral()*f - sum(f))
         	norm(Derivative()*f-f')
-        	norm(differentiate(integrate(f)) - f)
-        	norm(differentiate(cumsum(f))-f)
         	norm(first(cumsum(f)))
             for spfn in spfns
                 spfn(f)
