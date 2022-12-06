983b7a8c5246206845a5e274ae442e9a1b54d597
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-12-07 00:23:54 +0400

    trim trailing whitespace

--- test/MultivariateTest.jl
+++ test/MultivariateTest.jl
@@ -72,7 +72,7 @@ using Base: oneto
     end
 
     @testset "Evaluation" begin
-        
+
         @testset "2D" begin
             f2 = Fun(Chebyshev()^2, [1.0])
             @test f2(0.2, 0.4) == 1.0
@@ -88,7 +88,7 @@ using Base: oneto
             @test f20(rand(20)) == 1.0
         end
     end
-    
+
     @testset "Square" begin
         S = Space(ChebyshevInterval()^2)
         @test @inferred(blocklengths(S)) ≡ oneto(∞)
@@ -149,39 +149,39 @@ using Base: oneto
             # coefficients
              c_1 = rand(20)
              c_2 = rand(30)
- 
+
              added_coef = [c_2[1:20]+c_1;c_2[21:end]]
- 
+
              # 2D
              f2_1 = Fun(Chebyshev()^2, c_1)
              f2_2 = Fun(Chebyshev()^2, c_2)
              @test coefficients(f2_1+f2_2) == added_coef
- 
+
              @test (f2_1+f2_2)(0.3, 0.5)≈f2_1(0.3, 0.5)+f2_2(0.3, 0.5)
- 
+
              # 3D
              f3_1 = Fun(Chebyshev()^3, c_1)
              f3_2 = Fun(Chebyshev()^3, c_2)
              @test coefficients(f3_1+f3_2) == added_coef
- 
+
              @test (f3_1+f3_2)(0.3, 0.5, 0.6)≈f3_1(0.3, 0.5, 0.6)+f3_2(0.3, 0.5, 0.6)
          end
- 
+
          @testset "Multiplication" begin
              # coefficients
              c_1 = rand(20)
              c_2 = rand(30)
- 
+
              # 2D
              f2_1 = Fun(Chebyshev()^2, c_1)
              f2_2 = Fun(Chebyshev()^2, c_2)
- 
+
              @test (f2_1 * f2_2)(0.4, 0.5) ≈ f2_1(0.4, 0.5) * f2_2(0.4, 0.5)
- 
+
              # 3D: not implemented in code yet
              #f3_1 = Fun(Chebyshev()^3, c_1)
              #f3_2 = Fun(Chebyshev()^3, c_2)
- 
+
              #@test (f3_1*f3_2)(0.4,0.5,0.6) ≈ f3_1(0.4,0.5,0.6)*f3_2(0.4,0.5,0.6)
          end
     end
