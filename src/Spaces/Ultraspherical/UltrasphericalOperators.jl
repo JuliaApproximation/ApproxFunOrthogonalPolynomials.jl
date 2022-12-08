85b476a9d2d0b3ce9607483689c36168b49e419e
Jishnu Bhattacharya <jishnub.github@gmail.com>
2022-12-08 22:50:33 +0400

    Add tests

--- src/Spaces/Ultraspherical/UltrasphericalOperators.jl
+++ src/Spaces/Ultraspherical/UltrasphericalOperators.jl
@@ -248,6 +248,7 @@ end
 
 
 function coefficients(g::AbstractVector,sp::Ultraspherical{Int},C::Chebyshev)
+    domainscompatible(C,sp) || throw(ArgumentError("domains don't match: $(domain(sp)) and $(domain(C))"))
     if order(sp) == 1
         ultraiconversion(g)
     else
@@ -256,6 +257,7 @@ function coefficients(g::AbstractVector,sp::Ultraspherical{Int},C::Chebyshev)
     end
 end
 function coefficients(g::AbstractVector,C::Chebyshev,sp::Ultraspherical)
+    domainscompatible(C,sp) || throw(ArgumentError("domains don't match: $(domain(C)) and $(domain(sp))"))
     if order(sp) == 1
         ultraconversion(g)
     else
