aac33c7ee9ed08b18687600ee5d680486ebaa76c
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-02-22 10:09:51 +0400

    Add evaluation and multiplication tests

--- test/UltrasphericalTest.jl
+++ test/UltrasphericalTest.jl
@@ -161,57 +161,57 @@ using Static
         @test f(0.1, 0.2) ≈ im*exp(0.1)*sinpi(0.2)
     end
 
-    # @testset "Evaluation" begin
-    #     c = [i^2 for i in 1:4]
-    #     @testset for d in (0..1, ChebyshevInterval()), order in (1, 2, 0.5)
-    #         @testset for _sp in (Ultraspherical(order), Ultraspherical(order,d)),
-    #                 sp in (_sp, NormalizedPolynomialSpace(_sp))
-    #             d = domain(sp)
-    #             f = Fun(sp, c)
-    #             for ep in (leftendpoint, rightendpoint),
-    #                     ev in (ApproxFunBase.ConcreteEvaluation, Evaluation)
-    #                 E = @inferred ev(sp, ep, 0)
-    #                 @test E[2:4] ≈ E[1:4][2:end]
-    #                 @test E[1:2:5] ≈ E[1:5][1:2:5]
-    #                 @test E[2:2:6] ≈ E[1:6][2:2:6]
-    #                 @test Number(E * f) ≈ f(ep(d))
-    #                 E2 = @inferred ev(sp, ep(d), 0)
-    #                 @test Number(E2 * f) ≈ f(ep(d))
+    @testset "Evaluation" begin
+        c = [i^2 for i in 1:4]
+        @testset for d in (0..1, ChebyshevInterval()), order in (1, 2, 0.5)
+            @testset for _sp in (Ultraspherical(order), Ultraspherical(order,d)),
+                    sp in (_sp, NormalizedPolynomialSpace(_sp))
+                d = domain(sp)
+                f = Fun(sp, c)
+                for ep in (leftendpoint, rightendpoint),
+                        ev in (ApproxFunBase.ConcreteEvaluation, Evaluation)
+                    E = @inferred ev(sp, ep, 0)
+                    @test E[2:4] ≈ E[1:4][2:end]
+                    @test E[1:2:5] ≈ E[1:5][1:2:5]
+                    @test E[2:2:6] ≈ E[1:6][2:2:6]
+                    @test Number(E * f) ≈ f(ep(d))
+                    E2 = @inferred ev(sp, ep(d), 0)
+                    @test Number(E2 * f) ≈ f(ep(d))
 
-    #                 D = @inferred ev(sp, ep, 1)
-    #                 @test D[2:4] ≈ D[1:4][2:end]
-    #                 @test D[1:2:5] ≈ D[1:5][1:2:5]
-    #                 @test D[2:2:6] ≈ D[1:6][2:2:6]
-    #                 @test Number(D * f) ≈ f'(ep(d))
-    #                 Dp = @inferred ev(sp, ep(d), 1)
-    #                 @test Number(Dp * f) ≈ f'(ep(d))
+                    D = @inferred ev(sp, ep, 1)
+                    @test D[2:4] ≈ D[1:4][2:end]
+                    @test D[1:2:5] ≈ D[1:5][1:2:5]
+                    @test D[2:2:6] ≈ D[1:6][2:2:6]
+                    @test Number(D * f) ≈ f'(ep(d))
+                    Dp = @inferred ev(sp, ep(d), 1)
+                    @test Number(Dp * f) ≈ f'(ep(d))
 
-    #                 D2 = @inferred ev(sp, ep, 2)
-    #                 @test D2[2:4] ≈ D2[1:4][2:end]
-    #                 @test D2[1:2:5] ≈ D2[1:5][1:2:5]
-    #                 @test D2[2:2:6] ≈ D2[1:6][2:2:6]
-    #                 @test Number(D2 * f) ≈ f''(ep(d))
-    #                 D2p = @inferred ev(sp, ep(d), 2)
-    #                 @test Number(D2p * f) ≈ f''(ep(d))
+                    D2 = @inferred ev(sp, ep, 2)
+                    @test D2[2:4] ≈ D2[1:4][2:end]
+                    @test D2[1:2:5] ≈ D2[1:5][1:2:5]
+                    @test D2[2:2:6] ≈ D2[1:6][2:2:6]
+                    @test Number(D2 * f) ≈ f''(ep(d))
+                    D2p = @inferred ev(sp, ep(d), 2)
+                    @test Number(D2p * f) ≈ f''(ep(d))
 
-    #                 D3 = @inferred ev(sp, ep, 3)
-    #                 @test D3[2:4] ≈ D3[1:4][2:end]
-    #                 @test D3[1:2:5] ≈ D3[1:5][1:2:5]
-    #                 @test D3[2:2:6] ≈ D3[1:6][2:2:6]
-    #                 @test Number(D3 * f) ≈ f'''(ep(d))
-    #                 D3p = @inferred ev(sp, ep(d), 3)
-    #                 @test Number(D3p * f) ≈ f'''(ep(d))
-    #             end
-    #         end
-    #     end
-    # end
+                    D3 = @inferred ev(sp, ep, 3)
+                    @test D3[2:4] ≈ D3[1:4][2:end]
+                    @test D3[1:2:5] ≈ D3[1:5][1:2:5]
+                    @test D3[2:2:6] ≈ D3[1:6][2:2:6]
+                    @test Number(D3 * f) ≈ f'''(ep(d))
+                    D3p = @inferred ev(sp, ep(d), 3)
+                    @test Number(D3p * f) ≈ f'''(ep(d))
+                end
+            end
+        end
+    end
 
-    # @testset "Multiplication" begin
-    #     M = Multiplication(Fun(), Ultraspherical(0.5))
-    #     f = Fun(Ultraspherical(0.5))
-    #     f2 = Fun(x->x^2, Ultraspherical(0.5))
-    #     @test M * f ≈ f2
-    # end
+    @testset "Multiplication" begin
+        M = Multiplication(Fun(), Ultraspherical(0.5))
+        f = Fun(Ultraspherical(0.5))
+        f2 = Fun(x->x^2, Ultraspherical(0.5))
+        @test M * f ≈ f2
+    end
 
     # @testset "Integral" begin
     #     d = 0..1
