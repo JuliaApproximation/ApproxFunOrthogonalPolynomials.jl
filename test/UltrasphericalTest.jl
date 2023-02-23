edd73c7fb314b4ec63177e6d25a4aa57b3ccc896
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-02-23 10:32:01 +0400

    Add third ConcreteEvaluation tests

--- test/UltrasphericalTest.jl
+++ test/UltrasphericalTest.jl
@@ -169,7 +169,6 @@ using Static
                 d = domain(sp)
                 f = Fun(sp, c)
                 for ep in (leftendpoint, rightendpoint),
-                        # ev in (Evaluation,)
                         ev in (ApproxFunBase.ConcreteEvaluation, Evaluation)
                     E = @inferred ev(sp, ep, 0)
                     @test E[2:4] ≈ E[1:4][2:end]
@@ -187,13 +186,13 @@ using Static
                     Dp = @inferred ev(sp, ep(d), 1)
                     @test Number(Dp * f) ≈ f'(ep(d))
 
-                    # D2 = @inferred ev(sp, ep, 2)
-                    # @test D2[2:4] ≈ D2[1:4][2:end]
-                    # @test D2[1:2:5] ≈ D2[1:5][1:2:5]
-                    # @test D2[2:2:6] ≈ D2[1:6][2:2:6]
-                    # @test Number(D2 * f) ≈ f''(ep(d))
-                    # D2p = @inferred ev(sp, ep(d), 2)
-                    # @test Number(D2p * f) ≈ f''(ep(d))
+                    D2 = @inferred ev(sp, ep, 2)
+                    @test D2[2:4] ≈ D2[1:4][2:end]
+                    @test D2[1:2:5] ≈ D2[1:5][1:2:5]
+                    @test D2[2:2:6] ≈ D2[1:6][2:2:6]
+                    @test Number(D2 * f) ≈ f''(ep(d))
+                    D2p = @inferred ev(sp, ep(d), 2)
+                    @test Number(D2p * f) ≈ f''(ep(d))
 
                     # D3 = @inferred ev(sp, ep, 3)
                     # @test D3[2:4] ≈ D3[1:4][2:end]
