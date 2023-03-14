fdc8fe0a3cacee01050502091d2d2387042526e9
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-03-14 07:36:03 +0530

    Relax Ultraspherical ConcreteConversion order types (#217)
    * Relax Ultraspherical ConcreteConversion order types
    
    * union of tuples in bandwidths
    
    * Disable CI on Windows

--- src/fastops.jl
+++ src/fastops.jl
@@ -10,8 +10,9 @@
 # Conversions
 #####
 
-function BandedMatrix(S::SubOperator{T,ConcreteConversion{Chebyshev{DD,RR},Ultraspherical{Int,DD,RR},T},
-                              NTuple{2,UnitRange{Int}}}) where {T,DD,RR}
+function BandedMatrix(S::SubOperator{T,ConcreteConversion{Chebyshev{DD,RR},
+                Ultraspherical{LT,DD,RR},T},
+            NTuple{2,UnitRange{Int}}}) where {T,LT<:Union{Integer, StaticInt},DD,RR}
     # we can assume order is 1
     ret = BandedMatrix{eltype(S)}(undef, size(S), bandwidths(S))
     kr,jr = parentindices(S)
@@ -31,8 +32,8 @@ function BandedMatrix(S::SubOperator{T,ConcreteConversion{Chebyshev{DD,RR},Ultra
     ret
 end
 
-function BandedMatrix(V::SubOperator{T,ConcreteConversion{Ultraspherical{LT,DD,RR},Ultraspherical{LT,DD,RR},T},
-                                                                  NTuple{2,UnitRange{Int}}}) where {T,LT,DD,RR}
+function BandedMatrix(V::SubOperator{T,ConcreteConversion{Ultraspherical{LT1,DD,RR},Ultraspherical{LT2,DD,RR},T},
+                                                                  NTuple{2,UnitRange{Int}}}) where {T,LT1,LT2,DD,RR}
 
     n,m = size(V)
     V_l, V_u = bandwidths(V)
