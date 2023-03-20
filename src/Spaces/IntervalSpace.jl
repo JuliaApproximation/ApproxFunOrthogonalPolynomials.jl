f4d3f3e5a76514be09b65275cec09cc4ef9fc54e
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-03-20 20:23:09 +0530

    accept numbers as integral orders

--- src/Spaces/IntervalSpace.jl
+++ src/Spaces/IntervalSpace.jl
@@ -8,4 +8,7 @@ Fun(::typeof(identity), d::IntervalOrSegment{<:Number}) =
     Fun(Chebyshev(d), [mean(d), complexlength(d)/2])
 
 # the default domain space is higher to avoid negative ultraspherical spaces
-Integral(d::IntervalOrSegment,n::Integer) = Integral(Ultraspherical(1,d), n)
+function Integral(d::IntervalOrSegment, n::Number)
+    assert_integer(n)
+    Integral(Ultraspherical(1,d), n)
+end
