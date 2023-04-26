803d0fc4c733d0b8a323f0c65fb1e222d387dd8b
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-04-26 18:21:33 +0530

    Fix `conversion_type` for `NormalizedPolynomialSpace` (#243)
    * Fix conversion_type for NormalizedPolynomialSpace
    
    * don't return nospace
    
    * update test

--- src/Spaces/PolynomialSpace.jl
+++ src/Spaces/PolynomialSpace.jl
@@ -476,7 +476,13 @@ function Fun(::typeof(identity), S::NormalizedPolynomialSpace)
 end
 
 function conversion_rule(a::NormalizedPolynomialSpace{S}, b::S) where S<:PolynomialSpace
-    domainscompatible(domain(a), domain(b)) ? a : NoSpace()
+    asp = a.space
+    c = conversion_type(asp, b)
+    if c == asp
+        return a
+    else
+        return c
+    end
 end
 
 bandwidths(C::ConcreteConversion{NormalizedPolynomialSpace{S,D,R},S}) where {S,D,R} = (0, 0)
