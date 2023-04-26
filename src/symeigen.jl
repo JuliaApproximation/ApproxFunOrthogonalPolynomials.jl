399cdca2dd41c50516fc77b51e3f33d74cd0fa29
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-04-26 19:42:33 +0530

    update docstrings

--- src/symeigen.jl
+++ src/symeigen.jl
@@ -88,15 +88,14 @@ function bandmatrices_eigen(S::SymmetricEigensystem, n::Integer)
 end
 
 """
-    bandmatrices_eigen(S::Union{SymmetricEigensystem, SkewSymmetricEigensystem}, n::Integer)
+    bandmatrices_eigen(S::SymmetricEigensystem, n::Integer)
 
-Recast the symmetric/skew-symmetric eigenvalue problem `L v = λ v` subject to `B v = 0` to the generalized
-eigenvalue problem `SA v = λ SB v`, where `SA` and `SB` are banded operators, and
+Recast the self-adjoint eigenvalue problem `L v = λ v` subject to `B v = 0` to the generalized
+eigenvalue problem `SA v = λ SB v`, where `SA` and `SB` are symmetric banded operators, and
 return the `n × n` matrix representations of `SA` and `SB`.
-If `S isa SymmetricEigensystem`, the returned matrices will be `Symmetric`.
 
 !!! note
-    No tests are performed to assert that the system is symmetric/skew-symmetric, and it's the user's responsibility
+    No tests are performed to assert that the system is self-adjoint, and it's the user's responsibility
     to ensure that the operators are compliant.
 """
 bandmatrices_eigen(S::EigenSystem, n::Integer) = _bandmatrices_eigen(S, n)
