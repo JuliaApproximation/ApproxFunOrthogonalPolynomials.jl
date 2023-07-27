23a3ed74608d9e62ae655c3747833e5eba4a23ae
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-07-27 22:25:07 +0530

    Fix show

--- test/showtest.jl
+++ test/showtest.jl
@@ -14,12 +14,12 @@ using Static
 	@test repr(Jacobi(1,2)) == "Jacobi(1,2)"
 	@test repr(Jacobi(1.0,2.0)) == "Jacobi(1.0,2.0)"
 
-	@test repr(Chebyshev(0..1)) == "Chebyshev(0..1)"
-	@test repr(NormalizedChebyshev(0..1)) == "NormalizedChebyshev(0..1)"
-	@test repr(Ultraspherical(1,0..1)) == "Ultraspherical(1,0..1)"
-	@test repr(Legendre(0..1)) == "Legendre(0..1)"
-	@test repr(Jacobi(1,2,0..1)) == "Jacobi(1,2,0..1)"
-	@test repr(Jacobi(1.0,2.0,0..1)) == "Jacobi(1.0,2.0,0..1)"
+	@test repr(Chebyshev(0..1)) == "Chebyshev($(0..1))"
+	@test repr(NormalizedChebyshev(0..1)) == "NormalizedChebyshev($(0..1))"
+	@test repr(Ultraspherical(1,0..1)) == "Ultraspherical(1,$(0..1))"
+	@test repr(Legendre(0..1)) == "Legendre($(0..1))"
+	@test repr(Jacobi(1,2,0..1)) == "Jacobi(1,2,$(0..1))"
+	@test repr(Jacobi(1.0,2.0,0..1)) == "Jacobi(1.0,2.0,$(0..1))"
 
 	io = IOBuffer()
 	@testset "Derivative" begin
