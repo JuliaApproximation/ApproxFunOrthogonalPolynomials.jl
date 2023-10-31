9c3caab2151f0f16022c8c2e39d46208fb0bf142
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-10-31 15:20:39 +0530

    Add comment

--- ext/ApproxFunOrthogonalPolynomialsPolynomialsExt.jl
+++ ext/ApproxFunOrthogonalPolynomialsPolynomialsExt.jl
@@ -2,6 +2,8 @@ module ApproxFunOrthogonalPolynomialsPolynomialsExt
 
 using ApproxFunOrthogonalPolynomials
 import Polynomials
+# Specifying the full namespace is necessary because of https://github.com/JuliaLang/julia/issues/48533
+# See https://github.com/JuliaStats/LogExpFunctions.jl/pull/63
 import ApproxFunOrthogonalPolynomials.ApproxFunBase: Fun
 
 Fun(C::Polynomials.ChebyshevT, s::Chebyshev{<:ChebyshevInterval}) =
