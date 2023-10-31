43957bbb49def092483ca3ed8c1c3e0a31564bd0
Jishnu Bhattacharya <jishnub.github@gmail.com>
2023-10-31 15:05:47 +0530

    Import deps transitively in extension

--- ext/ApproxFunOrthogonalPolynomialsPolynomialsExt.jl
+++ ext/ApproxFunOrthogonalPolynomialsPolynomialsExt.jl
@@ -2,7 +2,7 @@ module ApproxFunOrthogonalPolynomialsPolynomialsExt
 
 using ApproxFunOrthogonalPolynomials
 import Polynomials
-import ApproxFunBase: Fun
+import ApproxFunOrthogonalPolynomials.ApproxFunBase: Fun
 
 Fun(C::Polynomials.ChebyshevT, s::Chebyshev{<:ChebyshevInterval}) =
 	Fun(s, float.(Polynomials.coeffs(C)))
